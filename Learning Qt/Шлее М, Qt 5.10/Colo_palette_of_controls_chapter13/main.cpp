2e517e21a1c1ffae3c1a175bd938f4a79f661c6f
Nrt <44117788+Northell@users.noreply.github.com>
2021-02-12 21:04:46 +0500

    Add files via upload

---
+++ Learning Qt/Шлее М, Qt 5.10/Colo_palette_of_controls_chapter13/main.cpp
@@ -0,0 +1,28 @@
+
+//Листинг 13.1, 13.2  Изменение палитры, Установка палитры для всего приложения
+#include <QApplication>
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QSpinBox spb;
+
+    QPalette pal = spb.palette();
+
+    pal.setBrush(QPalette::Button, QBrush(Qt::red, Qt::Dense3Pattern));
+    pal.setColor(QPalette::ButtonText, Qt::blue);
+    pal.setColor(QPalette::Text, Qt::magenta);
+    pal.setColor(QPalette::Active, QPalette::Base, Qt::green);
+
+    spb.setPalette(pal);
+    spb.resize(150,74);
+    spb.show();
+
+    //13.2 Установка палитры для всего приложения
+    QApplication::setPalette(pal);
+    //=============
+
+    a.setStyle("Windows");
+    return a.exec();
+}
